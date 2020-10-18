590c0db5018c049f60b940974fc088eb0011254f
ojhalakshya <ojhalakshya@gmail.com>
2020-10-18 17:49:38 +0530

    using functions

--- Phase-2/src/index.js
+++ Phase-2/src/index.js
@@ -4,5 +4,15 @@ function greet(name, lastName)
   console.log("Hello " + name + " " + lastName);
 }
 
+function square(number)
+{
+  return number * number;
+}
+
 greet("Lakshya", "Ojha");
 greet("Kartik");
+
+let number = 4;
+number = square(number);
+console.log(number);
+console.log(square(3));
