acdb114662b84c2f2fc0df37f1d53a388c1c685f
ojhalakshya <ojhalakshya@gmail.com>
2020-10-18 17:29:23 +0530

    inded.html and index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let person = {
+  name: "lakshya",
+  age: 22,
+  array: []
+};
+
+person.name = "Lakshya Ojha";
+person.array = ["red", "blue", 1];
+
+console.log(person);
+console.log(person.array.length);
